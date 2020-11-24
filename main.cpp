deeac79e565625d1324b495040a35d819f50b0fe
1249302623 <1249302623@qq.com>
2020-11-24 13:09:19 +0800

    master

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 //使用ab压力测试命令  ab -t 60 -n 2000000 -c 20000 -r -k http://127.0.0.1:8080/
 int main() {
-
     Singleton<AsyncLogging>::instance().startLog();
     EventLoop mainLoop;
     int connthreadNum=4,port=8080,workthreadNum=3,mysqlconnnum=5;
     Server myHTTPServer(&mainLoop, connthreadNum, port,workthreadNum,mysqlconnnum);
     myHTTPServer.start();
+
     //sqlPoolTest();
     //Buffer_test_main();
     //EvThread_test_main();
